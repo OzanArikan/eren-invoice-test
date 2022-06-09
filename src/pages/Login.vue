773fb281fab843f9824f7908b97efa47eccd2ed9
ozan arikan <oznarkn@gmail.com>
2022-06-10 01:39:52 +0300

    login screen properties has adjusted

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,9 @@
 <template>
     <!--<div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">-->
         <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
-            <div class="col-12 mt-5 xl:mt-0 text-center">
-              <!--  <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">-->
-            </div>
+            <!--<div class="col-12 md:col-6 mt-5 xl:mt-0 text-center">
+                <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">
+            </div>-->
             
             <!--<div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">-->
                 <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; ">
@@ -14,7 +14,7 @@
                         <span class="text-600 font-medium"> Giriş Yapınız </span>
                     </div>
                 
-                    <div class="w-full md:w-10 mx-auto">
+                    <div class="w-full md:w-4 mx-auto">
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Kullanıcı Adı</label>
                         <InputText id="email1" v-model="email" type="text" class="w-full mb-3" placeholder="Email" style="padding:1rem;" />
                 
@@ -33,7 +33,7 @@
                 </div>
             </div>
         <!--</div>-->
-    <!--</div>-->
+   <!-- </div>-->
 </template>
 
 <script>
